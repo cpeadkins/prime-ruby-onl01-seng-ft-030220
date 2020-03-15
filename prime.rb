1eadfa8e38de4d60c877e7b240dd12726cd1d79c
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 04:19:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@ def prime?(num)
   range = (2..num-1).to_a
     if num <= 0 || num ==1
       return false
-    elsif range.any? {|x| num % x == 0}
-      return false
-    else
-      return true
+    # elsif range.any? {|x| num % x == 0}
+    #   return false
+    # else
+    #   return true
     end
 end
